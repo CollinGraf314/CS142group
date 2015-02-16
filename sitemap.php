160495184a72303e1f70d5a48a29b91aa544d361
blazebot7000 <cgraf@uvm.edu>
2015-02-16 15:19:40 -0500

    original update
    .

---
+++ sitemap.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Collin Graf's Sitemap</title>
+<meta charset="utf-8">
+<meta name="author" content="Collin Graf">
+<meta name="description" content="">
+    
+<link rel="stylesheet"
+	  href="style.css"
+      media="screen">
+
+<!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+    
+</head>
+
+<body id="nameOfFile">
+<!-- ###########   comment to separate source code    ################## -->
+<header>
+    <h1>Collin Graf's Sitemap</h1>
+    <h3>Assignments</h3>
+</header>
+
+<!-- ###########   comment should have something     ################## -->
+<nav>
+     <ol>
+         <li>Assignment 1:</li>
+        <li><a href="assignment1/home.php">Assignment 1</a></li>
+        <li><a href="assignment1/form.css">style sheet</a></li>
+        <li><a href="assignment1/form2.css">alternate style sheet</a></li>
+     </ol>
+    <ol>
+         <li>Assignment 2:</li>
+        <li><a href="assignment2/resume.php">Assignment 2</a></li>
+        <li><a href="assignment2/style.css">style sheet</a></li>
+        <li><a href="assignment2/style2.css">alternate style sheet</a></li>
+        <li><a href="assignment2/print.css">print style sheet</a></li>
+     </ol>
+    <ol>
+         <li>Assignment 3:</li>
+        <li><a href="assignment3/table.php">Assignment 3</a></li>
+        <li><a href="assignment3/table.css">style sheet</a></li>
+        <li><a href="assignment3/table2.css">alternate style sheet</a></li>
+     </ol>
+    <ol>
+         <li>Assignment 4:</li>
+        <li><a href="assignment4/index.php">Assignment 4</a></li>
+        <li><a href="assignment4/cgraf.css">style sheet</a></li>
+        <li><a href="assignment4/cgraf_print.css">print style sheet</a></li>
+     </ol>
+</nav>
+
+<!-- ###########   meaningful about what this part   ################## -->
+
+<article id="main">
+
+
+</article>
+<!-- ###########  search for  html5 article vs section  ################## -->
+
+<!-- ###########   is all about                      ################## -->
+<aside id="rightColumn">
+
+</aside>
+
+<!-- ###########                                     ################## -->
+
+<footer>
+    <p>By: Collin Graf</p>
+</footer>
+
+</body>
+</html>
+            
